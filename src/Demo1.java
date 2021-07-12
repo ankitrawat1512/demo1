684f0632b719d25ce587ab71314783898a0b3ce0
Ankitrawat1512 <ankitrawat1512@gmail.com>
2021-07-12 12:46:44 -0400

    test commit and update

--- src/Demo1.java
+++ src/Demo1.java
@@ -4,7 +4,7 @@ public class Demo1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//testing for commit
 		
 		Scanner s =new Scanner(System.in);
 		System.out.println("Enter the no");
